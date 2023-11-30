75b78d7e3b3a844b349db477072ee0c70240b5ea
CarlosNico <132309481+CarlosNico@users.noreply.github.com>
2023-11-30 10:32:55 -0700

    Conexión y Tablas
    Actividad 2 - Lenguaje de Programación II

--- CarlosNicolini_A2.cpp
+++ CarlosNicolini_A2.cpp
@@ -3,70 +3,93 @@
 
 using namespace std;
 
-class Persona {
+class Tornillo {
 private: //Atributos
-	string nombre;
-	int edad;
+	string tipo;
+	float medida;
 public: //Metodos
-	Persona(string, int);
+	Tornillo(string, float);
 	virtual void mostrar();
 };
 
-Persona::Persona(string _nombre, int _edad) {
-	nombre = _nombre;
-	edad = _edad;
+Tornillo::Tornillo(string _tipo, float _medida) {
+	tipo = _tipo;
+	medida = _medida;
 }
 
-void Persona::mostrar() {
-	cout << "Nombre: " << nombre << endl;
-	cout << "Edad: " << edad << endl;
+void Tornillo::mostrar() {
+	cout << "Es un tornillo para: " << tipo << endl;
+	cout << "Tiene una medida de: " << medida << " cm" << endl;
 }
 
 
-class Alumno : public Persona {
+class Cabeza : public Tornillo {
 private:
-	float NotaFinal;
+	string forma;
 public:
-	Alumno(string, int, float);
+	Cabeza(string, float, string);
 	void mostrar();
 };
 
-Alumno::Alumno(string _nombre, int _edad, float _NotaFinal) : Persona(_nombre, _edad) {
-	NotaFinal = _NotaFinal;
+Cabeza::Cabeza(string _tipo, float _medida, string _forma) : Tornillo(_tipo, _medida) {
+	forma = _forma;
 }
 
-void Alumno::mostrar() {
-	Persona::mostrar();
-	cout << "Nota Final: " << NotaFinal << endl;
+void Cabeza::mostrar() {
+	Tornillo::mostrar();
+	cout << "Tipo de cabeza es: " << forma << endl;
 }
 
-class Profesor : public Persona {
+class Cantidad : public Tornillo {
 private:
-	string Materia;
+	int unidades;
 public:
-	Profesor(string, int, string);
+	Cantidad(string, float, int);
 	void mostrar();
 };
-Profesor::Profesor(string _nombre, int _edad, string _Materia) : Persona(_nombre, _edad) {
-	Materia = _Materia;
+Cantidad::Cantidad(string _tipo, float _medida, int _unidades) : Tornillo(_tipo, _medida) {
+	unidades = _unidades;
 }
 
-void Profesor::mostrar() {
-	Persona::mostrar();
-	cout << "La materia es: " << Materia << endl;
+void Cantidad::mostrar() {
+	Tornillo::mostrar();
+	cout << "El numero de unidades disponibles en almacen es: " << unidades << endl;
 }
+
+class Ubicacion : public Tornillo {
+private:
+	int pasillo;
+	string estante;
+public:
+	Ubicacion(string, float, int, string);
+	void mostrar();
+};
+Ubicacion::Ubicacion(string _tipo, float _medida, int _pasillo, string _estante) : Tornillo(_tipo, _medida) {
+	pasillo = _pasillo;
+	estante = _estante;
+}
+
+void Ubicacion::mostrar() {
+	Tornillo::mostrar();
+	cout << "Se encuentra en el pasillo: " << pasillo << endl;
+	cout << "En el estante: " << estante << endl;
+}
+
 int main() {
-	Persona* vector[3];
+	Tornillo* vector[4];
 
-	vector[0] = new Alumno("Carlos", 28, 8.9);
-	vector[1] = new Alumno("Brizeida", 19, 9.5);
-	vector[2] = new Profesor("Miguel", 38, "Matematica");
+	vector[0] = new Cabeza("Madera", 10.7, "Ranurada");
+	vector[1] = new Cabeza("Metal", 3.5, "Phillips");
+	vector[2] = new Cantidad("Madera", 10.7, 10000);
+	vector[3] = new Ubicacion("Metal", 3.5, 4, "C7");
 
 	vector[0]->mostrar();
 	cout << "\n" << endl;
 	vector[1]->mostrar();
 	cout << "\n" << endl;
 	vector[2]->mostrar();
+	cout << "\n" << endl;
+	vector[3]->mostrar();
 	cout << "\n" << endl;
 
 	system("pause");
